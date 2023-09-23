97960c34b1e401a5a4e546188346473c92a81229
Abhishek-Ravinuthala <115137736+Abhishek-Ravinuthala@users.noreply.github.com>
2023-09-23 11:07:43 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import SignIn from './pages/Authentication/SignIn';
 import SignUp from './pages/Authentication/SignUp';
 import Loader from './common/Loader';
 import routes from './routes';
+import PredictForm from './components/PredictForm'
 
 const DefaultLayout = lazy(() => import('./layout/DefaultLayout'));
 
@@ -24,9 +25,12 @@ function App() {
     <Toaster position='top-right' reverseOrder={false} containerClassName='overflow-auto'/>
   
       <Routes>
-        <Route path="/auth/signin" element={<SignIn />} />
-        <Route path="/auth/signup" element={<SignUp />} />
+       
+        
         <Route element={<DefaultLayout />}>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/predict" element={<PredictForm />} />
           <Route index element={<ECommerce />} />
           {routes.map(({ path, component: Component }) => (
             <Route
